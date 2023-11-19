09bccb6c3843e9519e30a21afc12c74fd53b4529
shannakay10 <143737955+shannakay10@users.noreply.github.com>
2023-11-19 18:26:23 -0500

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RewardValueTests {
 
@@ -14,17 +13,23 @@ public class RewardValueTests {
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        var rewardValue = new RewardValue(milesValue, true);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue * 0.0035;
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), 0.001);
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue, true);
+        double expectedCash = milesValue / 0.0035;
+        assertEquals(expectedCash, rewardValue.getCashValue(), 0.01);
     }
 }
